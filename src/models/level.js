3f7ec68d0fe325507355d56c461670f6bd293017
mentab <25363363+mentab@users.noreply.github.com>
2019-11-21 13:27:52 +0100

    split base modules

---
+++ src/models/level.js
@@ -0,0 +1,48 @@
+import { Player } from './../models/player.js';
+import { Coin } from './../models/coin.js';
+import { Monster } from './../models/monster.js';
+import { Lava } from './../models/lava.js';
+import { Vec } from './../models/vec.js';
+
+const levelChars = {
+  ".": "empty", "#": "wall", "+": "lava",
+  "@": Player, "o": Coin, "M": Monster,
+  "=": Lava, "|": Lava, "v": Lava
+};
+
+
+export class Level {
+  constructor(plan) {
+    let rows = plan.trim().split("\n").map(l => [...l]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch];
+        if (typeof type == "string") return type;
+        this.startActors.push(
+          type.create(new Vec(x, y), ch));
+        return "empty";
+      });
+    });
+  }
+}
+
+Level.prototype.touches = function(pos, size, type) {
+  let xStart = Math.floor(pos.x);
+  let xEnd = Math.ceil(pos.x + size.x);
+  let yStart = Math.floor(pos.y);
+  let yEnd = Math.ceil(pos.y + size.y);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let isOutside = x < 0 || x >= this.width ||
+                      y < 0 || y >= this.height;
+      let here = isOutside ? "wall" : this.rows[y][x];
+      if (here == type) return true;
+    }
+  }
+  return false;
+};
